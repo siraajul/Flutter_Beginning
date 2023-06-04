47abcee47fcf4fb4b18d68dcba292a2ee60bfb73
siraajul <shirajulislamparvez@gmail.com>
2023-06-04 23:31:48 +0600

    Project Begun

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: true,
-    home: Center(
-      child: Text('Hello Bangladesh'),
-    ),
-  ));
+  runApp(MyApp(appName: 'Chicken'));
+}
+
+class MyApp extends StatelessWidget {
+  final String appName;
+  const MyApp({required this.appName});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, title: appName, home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Intro to Widget'),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(),
+            Text(
+              'Hello World',
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'Hello World',
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'Hello World',
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ));
+  }
 }
